17ae55c6fe0e12a822721a6f160dca35a5977f8f
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2020-05-07 22:49:15 -0400

    test: storage and query tests

--- packages/ecs/src/__mocks__/storage.ts
+++ packages/ecs/src/__mocks__/storage.ts
@@ -1,18 +1,20 @@
 import { Storage } from "../storage"
 
-export function createStorage(): Storage {
-  let e = 0
-  return {
-    archetypes: [],
-    insert: jest.fn(),
-    create: jest.fn(() => e++),
-    destroy: jest.fn(),
-    addTag: jest.fn(),
-    removeTag: jest.fn(),
-    hasTag: jest.fn(() => true),
-    incrementVersion: jest.fn(),
-    patch: jest.fn(() => true),
-    findComponent: jest.fn(() => ({ _t: 0, _e: 0, _v: 0 } as any)),
-    registerComponentFactory: jest.fn(),
-  }
-}
+export const createStorage = jest.fn(
+  (): Storage => {
+    let e = 0
+    return {
+      archetypes: [],
+      insert: jest.fn(),
+      create: jest.fn(() => e++),
+      destroy: jest.fn(),
+      addTag: jest.fn(),
+      removeTag: jest.fn(),
+      hasTag: jest.fn(() => true),
+      incrementVersion: jest.fn(),
+      patch: jest.fn(() => true),
+      findComponent: jest.fn(() => ({ _t: 0, _e: 0, _v: 0 } as any)),
+      registerComponentFactory: jest.fn(),
+    }
+  },
+)
