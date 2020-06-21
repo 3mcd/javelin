25184c87119aceb8354b5722a2ac34b1d5949985
Robert <robm4958@gmail.com>
2020-06-21 16:09:43 -0400

    feat: partial #49 implement the topic logic (#71)
    * Partial #49 Implement the topic logic
    
    This commit implements the topic type and helper methods. Primarily
    focusing on the `createTopic`, which creates a topic with helper methods
    to push events, flush events, and read events.
    
    * remove old comments
    
    * Remove my mistakes that were made in the tsconfig.json files
    
    * QuickFixes
    
    - remove console.log from the .spec file
    - make the name property readOnly
    - swap flush method to use the mutableEmpty method from utils

---
+++ packages/ecs/src/topic.spec.ts
@@ -0,0 +1,23 @@
+import { Topic, createTopic } from "./topic"
+
+describe("createTopic", () => {
+  it("creates a new topic with the given name", () => {
+    type TestType = {
+      mass: Number
+    }
+    const topic = createTopic<TestType>("testTopic");
+    expect(topic.name).toBe("testTopic");
+  })
+
+  it("adds events to it's list of events", () => {
+    type TestType = {
+      mass: Number
+    }
+    const topic: Topic<string, TestType> = createTopic<TestType>("testTopic");
+    topic.pushEvent({ mass: 17 });
+
+    for (const event of topic) {
+      expect(event).toEqual({ mass: 17 });
+    }
+  })
+})
