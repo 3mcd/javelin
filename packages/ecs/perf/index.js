d62323bb3b5fbfdfa42c2d8ae1455f2d3bc36255
eric <eric.g.mcdaniel@gmail.com>
2021-02-20 22:43:26 -0500

    chore: fix perf script

--- packages/ecs/perf/index.js
+++ packages/ecs/perf/index.js
@@ -2,7 +2,6 @@ const hr = Array(40).fill("=").join("")
 
 const perfs = [
   { name: "perf_storage", run: require("./perf_storage").run },
-  { name: "perf_storage_pooled", run: require("./perf_storage_pooled").run },
 ]
 
 perfs.forEach(p => {
